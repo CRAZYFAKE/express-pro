bd75ae407590c780d4c24a2fc8e3d6947ca7e770
Crizer <553784520@qq.com>
2017-10-10 17:20:35 +0800

    update

--- app/models/hooker/update.js
+++ app/models/hooker/update.js
@@ -1,7 +1,12 @@
+/*
+ * @Author: yaoyixiang 
+ * @Date: 2017-10-10 17:19:52 
+ * @Last Modified by:   yaoyixiang 
+ * @Last Modified time: 2017-10-10 17:19:52 
+ */
 'use strict'
 
 const hooker = require('hooker')
-
 
 module.exports = (model) => {
     hooker.hook(model, ['update', 'findOneAndUpdate'], {
